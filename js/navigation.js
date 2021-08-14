f13c90c5c6dcb8680728db596c2ec0accc9ccf03
HENRY-KC <henrykc24@yahoo.com>
2021-08-14 18:59:46 +0100

    Correct linter error

--- js/navigation.js
+++ js/navigation.js
@@ -2,15 +2,9 @@ const listNav = document.querySelector('.list');
 const addNewNav = document.querySelector('.add-new');
 const contactNav = document.querySelector('.contact');
 
-const listNavText = document.querySelector('.list a');
-const addNewNavText = document.querySelector('.add-new a');
-const contactNavText = document.querySelector('.contact a');
-
 const book = document.querySelector('.all-books');
 const addBooks = document.querySelector('.add-books');
 const contactBox = document.querySelector('.contact-info');
-
-const listShow1 = document.querySelector('.listShow');
 
 listNav.addEventListener('click', () => {
   addBooks.style.display = 'none';
